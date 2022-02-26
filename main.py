83f9043068988463650648e520dc66d60ca24010
fishkadealer229 <94692853+fishkadealer229@users.noreply.github.com>
2022-02-26 22:24:29 +0300

    Captain+

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    user.surname = 'Scott'
+    user.name = "Ridley"
+    user.age = 21
+    user.position = 'captain'
+    user.speciality = 'research engineer'
+    user.address = 'module_1'
+    user.email = "scott_chief@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    user = User()
+    user.surname = 'Billy'
+    user.name = "Herringtone"
+    user.age = 43
+    user.position = 'master'
+    user.speciality = 'cleaning'
+    user.address = 'dungeon'
+    user.email = "billy_herringtone@gacha.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    user = User()
+    user.surname = 'Gafarov'
+    user.name = "Vadim"
+    user.age = 18
+    user.position = 'mayor'
+    user.speciality = 'programmer'
+    user.address = 'Enginerring'
+    user.email = "midav.06@mail.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+
+if __name__ == '__main__':
+    main()
